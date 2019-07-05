d7ccddb1232764b3056740ef037b0cf5ecc2240e
Sumia Akter <sillySumia@hotmail.com>
2019-07-05 15:42:31 +0100

    * Added job ordering function * Removed keyvaluepair as it cant be modified, used dictionary instead. * Added relevant tests

--- JobSequencer/JobSequencer.cs
+++ JobSequencer/JobSequencer.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace JobSequencerNS
 {
 
     public class JobSequencer
     {
+        private Dictionary<string, string> jobSeq;
+
         public JobSequencer()
         {
-            
+            this.jobSeq = new Dictionary<string, string>();
         }
 
         /// <summary>
@@ -19,7 +22,9 @@ namespace JobSequencerNS
         /// <returns></returns>
         public List<string> SortJobs(string input)
         {
-            var jobSeq = new List<KeyValuePair<string, string>>();
+            //clear previous data
+            jobSeq.Clear();
+
             var output = new List<string>();
 
             //check empty
@@ -28,8 +33,12 @@ namespace JobSequencerNS
                 return output;
             }
 
-            jobSeq = SplitJobs(input);
-            PrintSequence(jobSeq);
+            
+            SplitJobs(input);
+            OrderJob();
+            
+
+            //PrintSequence(jobSeq);
 
 
             //select values
@@ -37,16 +46,49 @@ namespace JobSequencerNS
             return output;
         }
 
+        private void OrderJob()
+        {
+            for(int i=0; i< jobSeq.Count; i++)
+            {
+                var job = jobSeq.ElementAt(i);
+            
+                //check self dependency
+                if (job.Key.Equals(job.Value))
+                {
+                    throw new Exception("Jobs can’t depend on themselves.");
+                }
 
-    
+                List<string> seq = new List<string>();
+                var dependentJob = job.Value;
+                while (!string.IsNullOrEmpty(dependentJob))
+                {
+                    if(seq.Contains(dependentJob))
+                    {
+                        throw new Exception("Jobs can’t have circular dependencies.");
+                    }
+
+                    seq.Add(dependentJob);
+                    var found = jobSeq.Where(x => x.Key.Equals(dependentJob)).FirstOrDefault();
+                    dependentJob = found.Key != null ? found.Value : string.Empty;                    
+                }
+
+
+                var seqStr = string.Join("", seq);
+                seq.ForEach(j => jobSeq.Remove(j));
+
+                jobSeq[job.Key] = seqStr;
+            }
+
+     
+        }
+
 
         /// <summary>
         /// coverts string to key/value pair list
         /// </summary>
         /// <param name="input"></param>
-        public List<KeyValuePair<string, string>> SplitJobs(string input)
+        private void SplitJobs(string input)
         {
-            var jobSeq = new List<KeyValuePair<string, string>>();
 
             var jobs = input.Split("\n");
             foreach(var seq in jobs)
@@ -55,15 +97,12 @@ namespace JobSequencerNS
                 var key = str.ElementAt(0).Trim();
                 var val = str.Length == 2 ? str.ElementAt(1).Trim() : string.Empty;
                 
-         
-                var pair = new KeyValuePair<string, string>(key, val);
-                jobSeq.Add(pair);
+                jobSeq.Add(key,val);
             }
 
-            return jobSeq;
         }
 
-        public void PrintSequence(List<KeyValuePair<string, string>> jobSeq)
+        public void PrintSequence(Dictionary<string, string> jobSeq)
         {
             foreach(var item in jobSeq)
             {
--- JobSequencerTests/JobSequencerTests.cs
+++ JobSequencerTests/JobSequencerTests.cs
@@ -58,21 +58,47 @@ namespace JobSequencerTests
 
 
         [Fact]
-        public void CheckJobDependancy()
+        public void CheckJobDependency()
         {
+            var input = "a =>\nb=>c\nc=>";
 
+            var sequencer = new JobSequencer();
+            var actualOutput = sequencer.SortJobs(input);
+
+            var expectedOutput = new List<string>() { "cb", "a" };
+            Assert.NotStrictEqual(expectedOutput, actualOutput);
         }
 
         [Fact]
-        public void CheckJobsNoSelfDependancy()
+        public void CheckJobDependency_2()
         {
+            var input = "a =>\nb=>c\nc=>f\nd => a\n e => b \n f=> ";
 
+            var sequencer = new JobSequencer();
+            var actualOutput = sequencer.SortJobs(input);
+
+            var expectedOutput = new List<string>() { "fcbe", "ad" };
+            Assert.NotStrictEqual(expectedOutput, actualOutput);
         }
 
         [Fact]
-        public void CheckJobsNoCircularDependancy()
+        public void CheckJobsNoSelfDependency()
         {
+            var input = "a =>\nb=>\nc=>c";
+            var sequencer = new JobSequencer();
+            
+            var ex = Assert.Throws<Exception>(() => sequencer.SortJobs(input));
+            Assert.Equal("Jobs can’t depend on themselves.", ex.Message);
+        }
 
+        [Fact]
+        public void CheckJobsNoCircularDependency()
+        {
+            var input = "a =>\nb=>c\nc=>f\nd=>a\ne =>\nf=>b";
+            var sequencer = new JobSequencer();
+
+            var ex = Assert.Throws<Exception>(() => sequencer.SortJobs(input));
+            Assert.Equal("Jobs can’t have circular dependencies.", ex.Message);
         }
     }
 }
